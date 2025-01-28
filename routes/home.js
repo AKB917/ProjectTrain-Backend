74d5c12ea7b27a2fd155c3ac7b21680ed87c70cb
Your name <Your email>
2025-01-28 16:14:06 +0100

    route get done for home

--- routes/home.js
+++ routes/home.js
@@ -25,54 +25,8 @@ router.get('/trips', (req, res) => {
             res.json({ result : false, error: "No trip found" })
         }
     })
-        
-
-    
 })
 
-// router.get('/trips', (req, res) => {
-// const departvalue = req.body.departure;
-// const arrivalvalue = req.body.arrival;
-// const datevalue = req.body.date;
 
-
-//     Trip.find()    
-//     .then(data => {
-//         const trips = data.filter((trip)=> 
-//             trip.departure === departvalue && 
-//             trip.arrival === arrivalvalue && 
-//             moment(trip.date).format('L') === datevalue
-//         ) ; 
-//         console.log('coucou',moment(data[0].date).format('L'))
-
-//             res.json({ result: true , trips })
-    
-//             }
-// )
-//    });
-//   ;
-// function date(){
-//    const  datea=new Date("2025-01-28T09:53:30.443Z").getFullYear().getMonth().getMonth().getMonth()
-// }
-
-
-//   router.post('/newtrip', (req,res) => {
-//     const newTrip = new Trip({
-//         departure: req.body.departure,
-// 	arrival: req.body.arrival,
-// 	date: req.body.date,
-// 	price: req.body.price,
-//     })
-//     newTrip.save()
-//         res.json({ result: true, allTrips });
-//   })
-
-
-
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
 module.exports = router;
