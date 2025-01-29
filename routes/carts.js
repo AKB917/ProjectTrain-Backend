cf8b408f45e52a99db7bd28ddeb82e1968588e89
Your name <Your email>
2025-01-29 15:25:01 +0100

    route/bookings post done

--- routes/carts.js
+++ routes/carts.js
@@ -74,6 +74,7 @@ router.get('/', async (req, res) => {
             totalPrice += trip.trip.price || 0; // Ajoute le prix au total, évite les valeurs undefined
             
             return {
+                id: trip.id,
                 departure: trip.trip.departure,
                 arrival: trip.trip.arrival,
                 time: time, // Ajoute l'heure formatée
